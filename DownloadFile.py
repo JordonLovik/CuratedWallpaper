c764f3427a0e61b25f0e49cee10830eb0bdba6fb
Jordon <jordonLovik@gmail.com>
2016-12-02 16:57:48 -0800

    Added Downloadsfile and main py

---
+++ DownloadFile.py
@@ -0,0 +1,7 @@
+#downloadimage from url
+import urllib.request
+website ="http://www.digimouth.com/news/media/2011/09/google-logo.jpg"
+#encode the following string because urllib.request does not like '/' characters
+LocalDir = "\CuratedWallpaper\Wallpaper.jpg"
+LocalDir2 = LocalDir.encode()
+urllib.request.urlretrieve(website, LocalDir2)
