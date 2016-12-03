92856776c5f3c9ace6a7dde4bfd1e04f549317f6
Jordon <jordonLovik@gmail.com>
2016-12-02 21:24:54 -0800

    created functions for changingBackground and making directory

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import os, sys
 import ctypes
 
 directory = "c:\CuratedWallpaper"
-
+imagePath = directory + "Mario.bmp"
 
 #makes Directory if dosent already exist
 def makedir(directory):
@@ -12,9 +12,16 @@ def makedir(directory):
         os.makedirs(folderpath)
         return;
 
+def changeBG(imagePath):
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imagePath , 0)
+    return;
+
+#makes diretory in C:\ if one is not already there
 makedir(directory)
 
-urllib.request.urlretrieve ("http://www.emunix.emich.edu/~evett/GameProgramming/BookCode/chapter11.new/timedloop/background.bmp", "\CuratedWallpaper\Mario.bmp")
+#takes in 1 argument: image file path and places it as background image
+changeBG(imagePath)
 
-SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "c:\CuratedWallpaper\Mario.bmp" , 0)
+#download an image
+urllib.request.urlretrieve ("http://www.emunix.emich.edu/~evett/GameProgramming/BookCode/chapter11.new/timedloop/background.bmp", "\CuratedWallpaper\Mario.bmp")
