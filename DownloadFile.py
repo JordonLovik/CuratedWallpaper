405517acbfbc19cb834615e7528c156aa9b7f6e3
Jordon <jordonLovik@gmail.com>
2016-12-02 17:20:02 -0800

    makedirectory functionality added to main.py

--- DownloadFile.py
+++ DownloadFile.py
@@ -1,5 +1,7 @@
 #downloadimage from url
 import urllib.request
+
+
 website ="http://www.digimouth.com/news/media/2011/09/google-logo.jpg"
 #encode the following string because urllib.request does not like '/' characters
 LocalDir = "\CuratedWallpaper\Wallpaper.jpg"
---
+++ Main.py
@@ -0,0 +1,7 @@
+import os, sys
+#makes Directory if dosent already exist
+directory = "c:\CuratedWallpaper"
+path = directory.encode()
+if not os.path.exists(path):
+    os.makedirs(path)
+
