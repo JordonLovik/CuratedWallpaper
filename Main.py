2f58befc39d8e9528850cdd7a83c8aceae74a620
Jordon <jordonLovik@gmail.com>
2017-01-05 09:17:27 -0800

    created array of strings to hold multiple URLS for downloading new images

--- Main.py
+++ Main.py
@@ -5,13 +5,19 @@ import os, sys
 import ctypes
 
 def main():
-    delay = 5
+    delay = 5 #time delay between
     #list of strings for local files
     photoAlbum = []
     photoAlbum.append(r"C:\CuratedWallpaper\1.jpg")
     photoAlbum.append(r"C:\CuratedWallpaper\2.jpg")
     photoAlbum.append(r"C:\CuratedWallpaper\3.jpg")
     photoAlbum.append(r"C:\CuratedWallpaper\4.jpg")
+
+    #List of strings for image urls for download
+    webUrl = []
+    webUrl.append(r"http://www.alsglobal.com/~/media/Images/Divisions/Life%20Sciences/Environmental/Houston.jpg")
+    webUrl.append(r"http://wallpaper-gallery.net/images/high-res-wallpaper/high-res-wallpaper-16.jpg")
+    webUrl.append(r"http://wallpaper-gallery.net/images/hi-res-wallpaper/hi-res-wallpaper-8.jpg")
 
     #main program loop
     directory = expanduser(r"~") + r"\Pictures" #directory is the home directory of computer plus /Picutres
@@ -35,11 +41,22 @@ def main():
     #main loop for changing bg
     for photos in photoAlbum:
         imagePath = photos
-        print(imagePath)
         changeBG(imagePath)
         time.sleep(delay)
 
+    #webUrl.append(r"http://www.alsglobal.com/~/media/Images/Divisions/Life%20Sciences/Environmental/Houston.jpg")
+    #webUrl.append(r"http://www.wearemoviegeeks.com/wp-content/uploads/WTCLibertyStatePark-HighRes-PhotoCreditErikaKoop-012.jpg")
 
+    #takes a url adds it to the webUrl array
+    def imagearray(webUrl):
+        for images in webUrl:
+            webUrl = images
+            print (webUrl)
+
+    imagearray(webUrl)
+
+
+    #"http://www.emunix.emich.edu/~evett/GameProgramming/BookCode/chapter11.new/timedloop/background.bmp", imagePath)
         #download an image to location
     #urllib.request.urlretrieve ("http://www.emunix.emich.edu/~evett/GameProgramming/BookCode/chapter11.new/timedloop/background.bmp", imagePath)
     #http://www.pixelstalk.net/wp-content/uploads/2016/06/High-resolution-highres-pictures.jpg
@@ -49,13 +66,5 @@ def main():
     #http://wallpaper-gallery.net/images/hi-res-wallpaper/hi-res-wallpaper-8.jpg
     #http://www.wearemoviegeeks.com/wp-content/uploads/WTCLibertyStatePark-HighRes-PhotoCreditErikaKoop-012.jpg
 
-   # while finish - start > 10:
-        #imagePath = photoAlbum. + r"\Water.jpg"
-       # changeBG(imagePath)
-    #else:
-        #imagePath = directory + r"\Mario.bmp"
-        #changeBG(imagePath)
 
-    #takes in 1 argument: image file path and places it as background image
-    #changeBG(imagePath)
 if __name__ == "__main__": main()
