c5249446727424975cf979d81b857fe3a5dedf5c
Jordon <jordonLovik@gmail.com>
2017-01-05 22:43:49 -0800

    not working however started the building blocks of a loop that will take a txt file with the urls of all images into an array then use the urllib.request.urlretrieve to dowload each image locally

--- Main.py
+++ Main.py
@@ -5,7 +5,8 @@ import os, sys
 import ctypes
 
 def main():
-    delay = 5 #time delay between
+    delay = 1 #time delay between
+    saveLocation = r"C:\CuratedWallpaper"
     #list of strings for local files
     photoAlbum = []
     photoAlbum.append(r"C:\CuratedWallpaper\1.jpg")
@@ -13,15 +14,13 @@ def main():
     photoAlbum.append(r"C:\CuratedWallpaper\3.jpg")
     photoAlbum.append(r"C:\CuratedWallpaper\4.jpg")
 
-    #List of strings for image urls for download
-    webUrl = []
-    webUrl.append(r"http://www.alsglobal.com/~/media/Images/Divisions/Life%20Sciences/Environmental/Houston.jpg")
-    webUrl.append(r"http://wallpaper-gallery.net/images/high-res-wallpaper/high-res-wallpaper-16.jpg")
-    webUrl.append(r"http://wallpaper-gallery.net/images/hi-res-wallpaper/hi-res-wallpaper-8.jpg")
-
-    #main program loop
     directory = expanduser(r"~") + r"\Pictures" #directory is the home directory of computer plus /Picutres
-    #imagePath = directory + r"\Mario.bmp"
+    imagePath = directory + r"\Mario.bmp"
+
+    #File with urls delimited by ','
+    f = open(r'C:\CuratedWallpaper\f.txt', 'r')
+    Urls = f.read().split(',')
+    f.close()
 
     #makes Directory if dosent already exist
     def makedir(directory):
@@ -35,29 +34,23 @@ def main():
         SPIF_UPDATEINIFILE = 0x2
         return;
 
-    #makes diretory in C:\ if one is not already there
-    makedir(directory)
+    #loop for saving images based on Urls array which is pulled from f.txt
+    for pics in Urls:
+        imagePath = pics
+        #urllib.request.urlretrieve (Urls,imagePath)
 
-    #main loop for changing bg
+    #loop for changing bg
     for photos in photoAlbum:
         imagePath = photos
         changeBG(imagePath)
         time.sleep(delay)
 
     #webUrl.append(r"http://www.alsglobal.com/~/media/Images/Divisions/Life%20Sciences/Environmental/Houston.jpg")
-    #webUrl.append(r"http://www.wearemoviegeeks.com/wp-content/uploads/WTCLibertyStatePark-HighRes-PhotoCreditErikaKoop-012.jpg")
-
-    #takes a url adds it to the webUrl array
-    def imagearray(webUrl):
-        for images in webUrl:
-            webUrl = images
-            print (webUrl)
-
-    imagearray(webUrl)
 
 
-    #"http://www.emunix.emich.edu/~evett/GameProgramming/BookCode/chapter11.new/timedloop/background.bmp", imagePath)
-        #download an image to location
+    # makes diretory in C:\ if one is not already there
+    makedir(directory)
+
     #urllib.request.urlretrieve ("http://www.emunix.emich.edu/~evett/GameProgramming/BookCode/chapter11.new/timedloop/background.bmp", imagePath)
     #http://www.pixelstalk.net/wp-content/uploads/2016/06/High-resolution-highres-pictures.jpg
     #http://www.alsglobal.com/~/media/Images/Divisions/Life%20Sciences/Environmental/Houston.jpg
@@ -65,6 +58,5 @@ def main():
     #http://wallpaper-gallery.net/images/high-res-wallpaper/high-res-wallpaper-16.jpg
     #http://wallpaper-gallery.net/images/hi-res-wallpaper/hi-res-wallpaper-8.jpg
     #http://www.wearemoviegeeks.com/wp-content/uploads/WTCLibertyStatePark-HighRes-PhotoCreditErikaKoop-012.jpg
-
 
 if __name__ == "__main__": main()
