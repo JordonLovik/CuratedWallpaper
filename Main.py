18e49558ff69b6f63b43eaccb5cf0cff94fb720c
Jordon <jordonLovik@gmail.com>
2017-01-09 15:10:41 -0800

    pulled changeBG from the download image for loop

--- Main.py
+++ Main.py
@@ -5,42 +5,43 @@ import os, sys
 import ctypes
 
 def main():
-    delay = 2 #time delay between
+    delay = 0.5
     saveLocation = r"C:\CuratedWallpaper\1"
-
+    localFiles = []
     #directory is the home directory of computer plus /Picutres
     directory = expanduser(r"~") + r"\Pictures"
 
-    #File with urls delimited by ','
     f = open(r'C:\CuratedWallpaper\f.txt', 'r')
     Urls = f.read().split('\n')
     f.close()
 
-    #makes Directory if dosent already exist
     def makedir(directory):
         if not os.path.exists(directory):
             os.makedirs(directory)
             return;
-    #changes background based on Path
     def changeBG(Path):
         SPI_SETDESKWALLPAPER = 20
         ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, Path , 0)
         SPIF_UPDATEINIFILE = 0x2
         return;
-    # makes diretory in C:\ if one is not already there
+
     makedir(directory)
 
+    #in this loop check to see if local file already exsists before doing the urllib.request
+
     #loop for saving images based on Urls array which is pulled from f.txt
-    for item in Urls:
-        filename = saveLocation + item.split('/')[-1]
-        urllib.request.urlretrieve(item, filename)
-        #outputString = path.abspath(filename)
-        print(filename)
-        print(item)
-        changeBG(filename)
-        time.sleep(delay)
+    for i in Urls:
+        filename = saveLocation + i.split('/')[-1]
+        localFiles.append(filename)
+        urllib.request.urlretrieve(i, filename)
+        #print(filename)
+        #print(i)
     else:
         pass
         #raise Exception("bad url")
+
+    for i in localFiles:
+        changeBG(i)
+        time.sleep(delay)
 
 if __name__ == "__main__": main()
