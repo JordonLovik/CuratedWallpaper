9e7568d28d7239ca9af923c801abf3aab48b455c
Jordon <jordonLovik@gmail.com>
2017-01-12 22:59:14 -0800

    Changed Directory to include users home\pictures\CuratedWallpaper

--- Main.py
+++ Main.py
@@ -1,17 +1,27 @@
 import urllib.request
 from os.path import expanduser
 import time
-import os, sys
+import os
 import ctypes
 
 def main():
-    delay = 0.5
-    saveLocation = r"C:\CuratedWallpaper\1"
-    localFiles = []
-    #directory is the home directory of computer plus /Picutres
-    directory = expanduser(r"~") + r"\Pictures"
+    delay = 1
 
-    f = open(r'C:\CuratedWallpaper\f.txt', 'r')
+    #for some reason th user input causes errors
+    #UserInput1 = int(input("Enter desired delay: "))
+    #print(UserInput1, type(UserInput1))
+    #delay = UserInput1
+
+    #Global Variables
+    #directory = expanduser(r"~") + r"\Pictures"
+    directory = expanduser(r"~") + r"\Pictures\CuratedWallpaper"
+    print (directory)
+    #saveLocation = r"C:\CuratedWallpaper\1"
+    saveLocation = directory + r"\CWP_"
+    localFiles = []
+
+    #f = open(r'C:\CuratedWallpaper\f.txt', 'r')
+    f = open(directory + r"\f.txt", 'r')
     Urls = f.read().split('\n')
     f.close()
 
@@ -24,22 +34,21 @@ def main():
         ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, Path , 0)
         SPIF_UPDATEINIFILE = 0x2
         return;
+    def DownloadImages(imageUrls):
+        for i in imageUrls:
+            filename = saveLocation + i.split('/')[-1]
+            localFiles.append(filename)
+            urllib.request.urlretrieve(i, filename)
+            #print(filename)
+            #print(i)
+        else:
+            pass
+            #raise Exception("bad url")
 
     makedir(directory)
+    DownloadImages(Urls)
 
-    #in this loop check to see if local file already exsists before doing the urllib.request
-
-    #loop for saving images based on Urls array which is pulled from f.txt
-    for i in Urls:
-        filename = saveLocation + i.split('/')[-1]
-        localFiles.append(filename)
-        urllib.request.urlretrieve(i, filename)
-        #print(filename)
-        #print(i)
-    else:
-        pass
-        #raise Exception("bad url")
-
+    #while loop will replace for loop in final program
     for i in localFiles:
         changeBG(i)
         time.sleep(delay)
