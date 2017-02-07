9bd02623b739f6214a1afe00165922ca7df83dc3
Jordon <jordonLovik@gmail.com>
2017-02-06 22:17:50 -0800

    looking at imageimport differently

--- ImageImport.py
+++ ImageImport.py
@@ -6,29 +6,50 @@
     save urls to f.txt file for later use
 """
 import urllib.request, urllib.error
+import urllib
 import http.client
 
-list = []
-listlenght = 3
+#list = []
+#listlenght = 3
+
+def is_downloadable(url):
+    h = urllib.request.Request((url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        print("false")
+        return False
+    if 'html' in content_type.lower():
+        print("false")
+        return False
+    print("True")
+    return True
+
+is_downloadable("http://wallpapercave.com/wp/46IcIP8.jpg")
+
+"""
+    def httpExists(url):
+        host, path = urlparse.urlsplit(url)[1:3]
+        found = 0
+        try:
+            connection = httplib.HTTPConnection(host)  ## Make HTTPConnection Object
+            connection.request("HEAD", path)
+            responseOb = connection.getresponse()  ## Grab HTTPResponse Object
+
+            if responseOb.status == 200:
+                found = 1
+            else:
+                print
+                "Status %d %s : %s" % (responseOb.status, responseOb.reason, url)
+        except Exception, e:
+            print
+            e.__class__, e, url
+        return found
+
+    def _test():
+        import doctest, httpExists
+        return doctest.testmod(httpExists)
 
 
-
-
-
-def ImageImport():
-    while len(list) < listlenght:
-        print(len(list))
-        item = input("enter urls: ")
-        c = http.client.HTTPSConnection(item)
-        c.request("HEAD", "/")
-        res = c.getresponse()
-        #print(res.status, res.reason)
-        data = res.reason
-        print(list)
-        #if state just wont work
-        if data == "OK":
-            list.pop()
-        else:
-            list.append(item)
-
-ImageImport()
+"""
+#objective take a url test to see if it is accpetable then add it to file coma delimited
