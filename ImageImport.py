5c882fded7e49632253bf2c9ca56f856e15a0516
Jordon <jordonLovik@gmail.com>
2017-02-12 00:47:44 -0800

    finished the image url checker function

--- ImageImport.py
+++ ImageImport.py
@@ -5,51 +5,27 @@
     store urls in list
     save urls to f.txt file for later use
 """
-import urllib.request, urllib.error
-import urllib
-import http.client
-
-#list = []
-#listlenght = 3
+import urllib.request
 
 def is_downloadable(url):
-    h = urllib.request.Request((url, allow_redirects=True)
-    header = h.headers
-    content_type = header.get('content-type')
-    if 'text' in content_type.lower():
-        print("false")
+    _url = url
+    req = urllib.request.Request(_url)
+    req.get_method = lambda: 'HEAD'
+    try:
+        urllib.request.urlopen(req)
+        print('sucess')
+        return True
+    except urllib.request.HTTPError:
+        print('failure')
         return False
-    if 'html' in content_type.lower():
-        print("false")
-        return False
-    print("True")
-    return True
 
-is_downloadable("http://wallpapercave.com/wp/46IcIP8.jpg")
+urllist = ["http://wallpapercave.com/wp/46IcIP8.jpg","http://wallpapercave.com/wp/46IcIP8.jpg","http://wallpapercave.com/wp/46IcIP8.jpg2","http://wallpapercave.com/wp/46IcIP8.jpg"]
+goodlist = []
+for i in urllist:
+    check = is_downloadable(i)
+    if check == True:
+        goodlist.append(i)
+    else:
+       print('an error occured when accessing a url')
 
-"""
-    def httpExists(url):
-        host, path = urlparse.urlsplit(url)[1:3]
-        found = 0
-        try:
-            connection = httplib.HTTPConnection(host)  ## Make HTTPConnection Object
-            connection.request("HEAD", path)
-            responseOb = connection.getresponse()  ## Grab HTTPResponse Object
-
-            if responseOb.status == 200:
-                found = 1
-            else:
-                print
-                "Status %d %s : %s" % (responseOb.status, responseOb.reason, url)
-        except Exception, e:
-            print
-            e.__class__, e, url
-        return found
-
-    def _test():
-        import doctest, httpExists
-        return doctest.testmod(httpExists)
-
-
-"""
-#objective take a url test to see if it is accpetable then add it to file coma delimited
+print(goodlist)
