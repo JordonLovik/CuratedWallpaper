461601d37b69a0f9d3ec96f50418a878d34ce323
Jordon <jordonLovik@gmail.com>
2017-03-06 22:35:41 -0800

    basic features are implemented. App now opens images from file.

--- Main.py
+++ Main.py
@@ -1,69 +1,95 @@
 import urllib.request
 from os.path import expanduser
-import time
 import os
+import time
 import ctypes
+import tkinter as tk
+from tkinter import filedialog
+
+#up next dowload image func implemented
+#store local images to be referenced
 
 def main():
-    #Global Variables
-    directory = expanduser(r'~') + r'\Pictures\CuratedWallpaper'
-    saveLocation = directory + r'\CWP_'
-    delay = 1
-    displayarray = []
-    goodurllist = []
+    #takes root directory and adds given folders to it.
 
-    file = open(directory + r'\f.txt', 'r')
-    urls = file.read().split('\n') #line delimite urls and store in array
-    file.close()
+    dir = expanduser(r'~') + r'\Pictures\CuratedWallpaper'
+    if not os.path.exists(dir):
+        os.makedirs(dir)#make directoy if does not exist already
+    #takes in directory location of files and adds prefix for saving local images to later
+    saveLocation = dir + r'\CWP_'
+    displayalbum = []  # array of file locations to be used to change BG
 
+    root = tk.Tk()
+    root.withdraw()
+    urlfile_path = filedialog.askopenfilename()
+    #urlfile = open(dir + r'\urlfile.txt', 'w') #previous open method
+    urlfile = open(urlfile_path, 'r')
+    urlslist = urlfile.read().split('\n') #line delimite urls and store in array
+    urlfile.close()
 
-    def makedir(directory):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    def changeBG(Path):
-        SPI_SETDESKWALLPAPER = 20
-        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, Path , 0)
-        SPIF_UPDATEINIFILE = 0x2
-    def downloadimages(imageurls):
-        _countertotal = len(imageurls)
-        _counter = 0
-        for i in imageurls:
-            filename = saveLocation + i.split('/')[-1]
-            displayarray.append(filename)
-            urllib.request.urlretrieve(i, filename)
-            _counter += 1
-            print("Downloading: {} of {}".format(_counter, _countertotal))
-    #build in more error checking. if line has no url etc
-    def is_downloadable(url):
-        _url = url
-        req = urllib.request.Request(_url)
+    storefile = open(dir + r'\storefile.txt', 'w')
+    if os.stat(dir + r'\storefile.txt').st_size != 0: #if storefile has data read it
+        imagestore = storefile.read().split('\n')  # line delimite urls and store in array
+    else:#if no data in imagestore create blank list
+        imagestore = []
+    running = True
+
+    def checklink(imageurl):
+        """takes in one url and checks it -
+            returns true or false"""
+        _imageurl = imageurl
+        req = urllib.request.Request(_imageurl)
         req.get_method = lambda: 'HEAD'
         try:
             urllib.request.urlopen(req)
-            goodurllist.append(_url)
-            print('{} succeeded'.format(_url))
+            #print('{} succeeded'.format(_imageurl))
+            return True
         except urllib.request.HTTPError:
-            print('{} failed'.format(_url))
+            #print('{} failed'.format(_imageurl))
+            return False
+    def changebg(imagepath):
+        """takes in an image and sets background to it"""
+        _imagepath = imagepath
+        SPI_SETDESKWALLPAPER = 20
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, _imagepath, 0)
+        SPIF_UPDATEINIFILE = 0x2
+    def downloadimage(imageurl, download = False):
+        """takes in one image and downloads it - saves file string to displayalbum -
+            stores list of downloaded images to list storefile.txt
+                Use the Download flag False image has already been downloaded subverting
+                    already downloaded images and storing into displayalbum"""
+        filename = saveLocation + str(imageurl.split('/')[-1]) #Strip url to just the last element and add to savelocation
+        if download == True:
+            urllib.request.urlretrieve(imageurl, filename) #download url to savelocation + striped filename
+            #print("Downloaded: " + imageurl + 'to: ' + filename)
+        displayalbum.append(filename) #append File name to display array for changing BG later
 
+    #while running:
+        # only run this for images that have not already been downloaded
+    for i in urlslist:
+        if i not in imagestore: # if image has already been dowloaded
+            if checklink(i) == True: #if check is sucessfull download image
+                downloadimage(i, True)
+                imagestore.append(i)
+                storefile.write(i + '\n')
+            else: pass #throw error to log and keep going
+        else: #case the images have been downloaded before
+            downloadimage(i, False)
+    #print(imagestore)
+    storefile.close()
 
+    delay = input('Enter transition time desired: ')
+    #setup delay here
+    for j in displayalbum:
+        #print(j)
+        changebg(j)
+        time.sleep(int(delay))
 
-    makedir(directory)
-    for i in urls:
-        is_downloadable(i)
-    """
-    print('*testing downloadability*')
-    for j in urls:
-        check = is_downloadable(j)
-        if check == True:
-            goodurllist.append(j)
-    """
-    downloadimages(goodurllist)
-
-    #downloadimages(rls) to see is_downloadable working
-
-    for i in displayarray:
-        changeBG(i)
-        time.sleep(delay)
+    #keep exicuting program?
+    answer = input("keep program running? y/n: ")
+    if answer == 'y':
+        pass
+    else:
+        running = False
 
 if __name__ == '__main__': main()
-
--- Main2.py
+++ Main2.py
@@ -1,24 +1,16 @@
 import urllib.request
 from os.path import expanduser
 import time
-import os
 import ctypes
 #up next dowload image func implemented
 #store local images to be referenced
 
 def main():
-
-    def openfile(directory, filename, accesstype, delimiter='\n'):
-        f = os.path.join(directory, filename)
-        file = open(f, accesstype)
-        filelist = file.read().split(delimiter)
-        file.close()
-        return filelist
-
+    #takes root directory and adds given folders to it.
     dir = expanduser(r'~') + r'\Pictures\CuratedWallpaper'
+    #takes in directory location of files and adds prefix for saving local images to later
     saveLocation = dir + r'\CWP_'
-
-    urls = openfile(dir, 'storefile.txt', 'r')
+    displayalbum = []  # array of file locations to be used to change BG
 
     urlfile = open(dir + r'\urlfile.txt', 'r')
     urlslist = urlfile.read().split('\n') #line delimite urls and store in array
@@ -26,10 +18,8 @@ def main():
 
     storefile = open(dir + r'\storefile.txt', 'r+')
     imagestore = storefile.read().split('\n')  # line delimite urls and store in array
-
     running = True
     localimages = ["1cwp.jpg", "2cwp.jpg"]
-    #imagestore = []
 
     """inicialize this list with downloadhistory.txt and save to it later on"""
     #downloadhistory [] = open(downloadhistory.txt)
@@ -42,10 +32,10 @@ def main():
         req.get_method = lambda: 'HEAD'
         try:
             urllib.request.urlopen(req)
-            print('{} succeeded'.format(_imageurl))
+            #print('{} succeeded'.format(_imageurl))
             return True
         except urllib.request.HTTPError:
-            print('{} failed'.format(_imageurl))
+            #print('{} failed'.format(_imageurl))
             return False
 
     def changebg(imagepath):
@@ -55,32 +45,39 @@ def main():
         ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, _imagepath, 0)
         SPIF_UPDATEINIFILE = 0x2
 
-    def downloadimage(image):
-        """takes in one image and downloads it - saves file xcwp.jpg -
-            stores list of downloaded images to list imagestore image"""
-        #download image
-        #save to location on computer
-        #store location of image in list imagestore
-        pass
+    def downloadimage(imageurl, download = False):
+        """takes in one image and downloads it - saves file string to displayalbum -
+            stores list of downloaded images to list storefile.txt
+                Use the Download flag False image has already been downloaded subverting
+                    already downloaded images and storing into displayalbum"""
+        filename = saveLocation + str(imageurl.split('/')[-1]) #Strip url to just the last element and add to savelocation
+        if download == True:
+            urllib.request.urlretrieve(imageurl, filename) #download url to savelocation + striped filename
+            #print("Downloaded: " + imageurl + 'to: ' + filename)
+        displayalbum.append(filename) #append File name to display array for changing BG later
+
 
     while running:
         # only run this for images that have not already been downloaded
-        for j in urlslist:
-            if j not in imagestore: # if image has already been dowloaded
-                if checklink(j) == True: #if check is sucessfull download image
-                    downloadimage(j)
-                    imagestore.append(j)
-                    storefile.write(j + '\n')
+        for i in urlslist:
+            if i not in imagestore: # if image has already been dowloaded
+                if checklink(i) == True: #if check is sucessfull download image
+                    downloadimage(i, True)
+                    imagestore.append(i)
+                    storefile.write(i + '\n')
                 else: pass #throw error to log and keep going
-            else: pass
-
+            else: #case the images have been downloaded before
+                downloadimage(i, False)
         #store already downloaded images to file
-        print(imagestore)
+        #print(imagestore)
         storefile.close()
 
+        delay = input('Enter transition time desired: ')
         #setup delay here
-        for x in imagestore:
-            changebg(x)
+        for j in displayalbum:
+            #print(j)
+            changebg(j)
+            time.sleep(int(delay))
 
         #keep exicuting program?
         answer = input("keep program running? y/n: ")
